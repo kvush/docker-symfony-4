89b045a19788f5440020da8910eb02153fce9826
Ilya Shashilov <kvushisha@gmail.com>
2019-03-07 14:59:52 +0700

    add env

--- docker/php/composer-installer.sh
+++ docker/php/composer-installer.sh
@@ -4,7 +4,7 @@ EXPECTED_SIGNATURE=$(curl -s https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+if [[ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]]
 then
     >&2 echo 'ERROR: Invalid installer signature'
     rm composer-setup.php
@@ -14,4 +14,4 @@ fi
 php composer-setup.php --quiet
 RESULT=$?
 rm composer-setup.php
-exit $RESULT
+exit ${RESULT}
